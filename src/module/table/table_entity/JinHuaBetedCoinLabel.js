cf4dd472cec9deedd3dd06f95ef2f9974d844bce
Long128821 <1757221175@qq.com>
2017-01-05 00:20:43 +0800

    1、头像旁边的下注模块(JinHuaBetedCoinLabel)添加注释; 2、ProfileJinHuaTable添加注释;

--- src/module/table/table_entity/JinHuaBetedCoinLabel.js
+++ src/module/table/table_entity/JinHuaBetedCoinLabel.js
@@ -1,35 +1,53 @@
 /**
  * Func:金花加注文本、背景
- * @type {{m_sprite: null, coinBg: null, coinImg: null, coinLabel: null, bg: null, create: create, setBetCoin: setBetCoin}}
+ * 继承cc.Node
  */
 var JinHuaBetedCoinLabel= cc.Node.extend({
     coinBg:null,//加注背景
-    coinImg:null,
-    coinLabel:null,
-    bg:null,
-    ctor:function(x,y){
+    coinIcon:null,//金币图标
+    coinLabel:null,//已加注金额文本
+    /**
+     * Func:构造函数(缺省)
+     * @param pos 坐标X或者坐标
+     * @param y 坐标Y
+     */
+    ctor:function(pos,y){
         this._super();
-        this.init(x, y);
+        (y!= undefined)&&(pos= cc.p(pos, y));
+        this.init(pos);
     },
-    init:function(x, y){
+    /**
+     * Func:初始化函数-加注金币模块(背景、金币图标、金币数量)
+     * @param pos cc.Node的位置坐标
+     */
+    init:function(pos){
+        //加注模块背景
         this.coinBg = cc.Sprite.create("#ui_paizhuo_gerenxiazhudi.png");
         this.coinBg.setPosition(35,20);
-
-        this.coinImg = cc.Sprite.create("#ic_gerenxiachoumatubiao.png");
-        this.coinImg.setPosition(this.coinBg.getContentSize().width / 10,this.coinBg.getContentSize().height / 2);
-        this.coinBg.addChild(this.coinImg);
-
-        this.coinLabel = cc.LabelTTF.create("", "Arial", 24);
-        this.coinLabel.setColor(cc.color(255, 255, 255));
-        this.coinLabel.setPosition(this.coinBg.getContentSize().width * 6 / 10,this.coinBg.getContentSize().height / 2);
-        this.coinBg.addChild(this.coinLabel);
-
         this.addChild(this.coinBg);
 
-        this.setAnchorPoint(cc.p(0, 0));
-        this.setPosition(x, y);
+        //背景尺寸
+        var bgSize= this.coinBg.getContentSize();
+
+        //金币图标
+        this.coinIcon = cc.Sprite.create("#ic_gerenxiachoumatubiao.png");
+        this.coinIcon.setPosition(bgSize.width / 10,bgSize.height / 2);
+        this.coinBg.addChild(this.coinIcon);
+
+        //金币数量
+        this.coinLabel = cc.LabelTTF.create("", "Arial", 24);
+        this.coinLabel.setColor(cc.color(255, 255, 255));//白色
+        this.coinLabel.setPosition(bgSize.width * 6 / 10,bgSize.height / 2);
+        this.coinBg.addChild(this.coinLabel);
+
+        this.setAnchorPoint(cc.p(0, 0));//锚点-左下角
+        this.setPosition(pos);
         this.setZOrder(30);
     },
+    /**
+     * Func:设置下注金额,如果参数不是有效的,隐藏该节点。
+     * @param value 下注金额
+     */
     setBetCoin:function(value){
         if(value== undefined||value== null||value< 0){
             this.coinLabel.setString(value);
@@ -38,5 +56,14 @@ var JinHuaBetedCoinLabel= cc.Node.extend({
         }else{
             this.setVisible(false);
         }
+    },
+    //释放-重载基类的release函数
+    release:function(){
+        this.coinLabel.removeFromParent(true);
+        this.coinIcon.removeFromParent(true);
+        this.coinBg.removeFromParent(true);
+        this.coinIcon= null;
+        this.coinLabel= null;
+        this.coinBg= null;
     }
 });
